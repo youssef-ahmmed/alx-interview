646ba7046ca4779234f73476be66ec1bacbfab2c
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-14 08:54:41 +0200

    Make getCharacterBody function return Promise

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,26 +4,34 @@ const args = process.argv;
 
 const getCharacters = (movieId) => {
   const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-  request({ url, json: true }, (error, response, body) => {
+
+  request({ url, json: true }, async (error, response, body) => {
     if (error) {
       console.error('Error:', error);
       return;
     }
+
     const charactersUrls = body.characters;
+
     for (const characterUrl of charactersUrls) {
-      getCharacterNames(characterUrl);
+      const characterBody = await getCharacterBody(characterUrl);
+      console.log(JSON.parse(characterBody).name);
     }
   });
 };
 
-const getCharacterNames = (characterUrl) => {
-  request(characterUrl, (error, response, body) => {
-    if (error) {
-      console.error('Error:', error);
-    }
-    console.log(JSON.parse(body).name);
+async function getCharacterBody (characterUrl) {
+  return new Promise((resolve, reject) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        reject(error);
+      }
+      if (response.statusCode === 200) {
+        resolve(body);
+      }
+    });
   });
-};
+}
 
 const movieId = args[2];
 getCharacters(movieId);
