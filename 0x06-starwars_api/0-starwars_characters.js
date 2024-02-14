0e18a84012d31abde27854c1db38e6e9293d240a
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-14 14:00:22 +0200

    Print Star Wars Character per line using SWAPI

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,28 +3,27 @@
 const request = require('request');
 const args = process.argv;
 
-const movieId = args[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+const getCharacters = (movieId) => {
+  const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-request.get(url, async (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
+  request({ url, json: true }, async (error, response, body) => {
+    if (error) {
+      console.error('Error:', error);
+      return;
+    }
 
-  if (response.statusCode === 200) {
-    const charactersUrls = JSON.parse(body).characters;
+    const charactersUrls = body.characters;
 
     for (const characterUrl of charactersUrls) {
       const characterBody = await getCharacterBody(characterUrl);
       console.log(JSON.parse(characterBody).name);
     }
-  }
-});
+  });
+};
 
 async function getCharacterBody (characterUrl) {
   return new Promise((resolve, reject) => {
-    request.get(characterUrl, (error, response, body) => {
+    request(characterUrl, (error, response, body) => {
       if (error) {
         reject(error);
       }
@@ -34,3 +33,6 @@ async function getCharacterBody (characterUrl) {
     });
   });
 }
+
+const movieId = args[2];
+getCharacters(movieId);
