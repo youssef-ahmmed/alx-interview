e99ff7f1fd0f6fd0bf91f2c8636059d82c894b50
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-14 08:59:29 +0200

    Remove getCharacters function

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,23 +2,22 @@
 const request = require('request');
 const args = process.argv;
 
-const getCharacters = (movieId) => {
-  const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+const movieId = args[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-  request({ url, json: true }, async (error, response, body) => {
-    if (error) {
-      console.error('Error:', error);
-      return;
-    }
+request({ url, json: true }, async (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
 
-    const charactersUrls = body.characters;
+  const charactersUrls = body.characters;
 
-    for (const characterUrl of charactersUrls) {
-      const characterBody = await getCharacterBody(characterUrl);
-      console.log(JSON.parse(characterBody).name);
-    }
-  });
-};
+  for (const characterUrl of charactersUrls) {
+    const characterBody = await getCharacterBody(characterUrl);
+    console.log(JSON.parse(characterBody).name);
+  }
+});
 
 async function getCharacterBody (characterUrl) {
   return new Promise((resolve, reject) => {
@@ -32,6 +31,3 @@ async function getCharacterBody (characterUrl) {
     });
   });
 }
-
-const movieId = args[2];
-getCharacters(movieId);
