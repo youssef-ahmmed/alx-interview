149082301bc02e92317ab48b44e6630d155ccc0d
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-14 08:39:23 +0200

    Fix semistandard errors

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,27 +4,26 @@ const args = process.argv;
 
 const getCharacters = (movieId) => {
   const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-  request({url, json: true}, (error, response, body) => {
+  request({ url, json: true }, (error, response, body) => {
     if (error) {
       console.error('Error:', error);
       return;
     }
-    const characters_urls = body.characters;
-    for (const character_url of characters_urls) {
-      getCharacterNames(character_url);
+    const charactersUrls = body.characters;
+    for (const characterUrl of charactersUrls) {
+      getCharacterNames(characterUrl);
     }
   });
 };
 
-const getCharacterNames = (character_url) => {
-  request(character_url, (error, response, body) => {
+const getCharacterNames = (characterUrl) => {
+  request(characterUrl, (error, response, body) => {
     if (error) {
       console.error('Error:', error);
     }
     console.log(JSON.parse(body).name);
   });
 };
-
 
 const movieId = args[2];
 getCharacters(movieId);
