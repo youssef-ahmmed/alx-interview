9745c0b97e74661f559ff794ec5ba8de745595bc
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-29 16:37:01 +0200

    Use temp variables instead of dict

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,49 +2,23 @@
 """Implement Making Change Algorithm"""
 
 
-def get_min_ignore_none(a, b):
-    """Get the min of 2 nums if not None"""
-    if not a:
-        return b
-    if not b:
-        return a
-
-    return min(a, b)
-
-
-# def makeChange(coins, total):
-#     """Making change using bottom-up approach"""
-#     cache = {0: 0}
-#
-#     for i in range(1, total + 1):
-#         cache[i] = float('inf')
-#         for coin in coins:
-#             subproblem = i - coin
-#
-#             if subproblem < 0:
-#                 continue
-#
-#             cache[i] = get_min_ignore_none(
-#                 cache.get(i),
-#                 cache.get(subproblem) + 1
-#             )
-#
-#     if cache[total] == float('inf'):
-#         return -1
-#
-#     return cache[total]
-
-
 def makeChange(coins, total):
-    """Making change using bottom-up approach"""
-    cache = {0: 0}
+    """ Generate changes needed to reach total"""
+    if total <= 0:
+        return 0
 
-    for i in range(1, total + 1):
-        cache[i] = float('inf')
+    check, steps = 0, 0
+    coins.sort(reverse=True)
 
     for coin in coins:
-        for i in range(coin, total + 1):
-            cache[i] = min(cache[i], cache[i - coin] + 1)
+        while check < total:
+            check += coin
+            steps += 1
 
-    return cache[total] if cache[total] != float('inf') else -1
+        if check == total:
+            return steps
 
+        check -= coin
+        steps -= 1
+
+    return -1
